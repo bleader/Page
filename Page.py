32e63b51676f9e573774066532b037afb7bfb6d7
bleader <bleader@ratonland.org>
2012-08-11 03:20:11 +0200

    init: create doc and add first version of module
    - filled README.md to make a small documentation
    - added the first version of the module Page.py
    
    Signed-off-by: bleader <bleader@ratonland.org>

---
+++ Page.py
@@ -0,0 +1,130 @@
+class Page:
+    """ Page class
+    This class is meant to be an abstract class to help generate webpage in
+    cherrypy (or other frameworks), by helping creating:
+    - header filled at instantiation
+    - body filling helpers
+    - block indentation
+    - default indentation is 4 spaces
+
+    Example Usage
+    -------------
+
+    import Page
+
+    p = Page()        # creates and fill the header open body tag
+    p.opn("<div>")    # open tag: insert and indent following insertions
+    p.add("content")  # add content
+    p.cls("</div>")   # close tag: deindent and insert closing tag
+    p.get()           # close body and html tags
+
+    """
+
+    # content of the page
+    _page = ""
+
+    # indent status and count per indent
+    _indent = ""
+    _indent_count = 4
+
+    # header
+    _doctype = "html"
+    _lang = "en"
+    _title = "title"
+    _content = "text/html"
+    _charset = "utf-8"
+    _keywords = []
+    _description = { 'en': "No description" }
+    _css = "/index.css"
+    _additionnal_headers = []
+
+    def set_indent(self, count):
+        """ Set indentation to count
+            @param count    The count of spaces to use for indentation
+        """
+        self._indent_count = count
+
+    def indent(self):
+        """indent text that will be inserted with the insertion methods"""
+        i = 0
+        while i < self._indent_count:
+            self._indent = self._indent + " "
+            i = i + 1
+
+    def deindent(self):
+        """Remove a level of indent to insertion methods"""
+        self._indent = self._indent[:-self._indent_count]
+
+    def add(self, line, newline=True):
+        """Add an indented line with current indentation
+           @param line      The line to be added
+           @param newline   Defines if newline is to be inserted
+        """
+        self._page = self._page + self._indent + line
+        if newline:
+            self._page = self._page + "\n"
+
+    def app(self, str):
+        """Append a string to the current line, mostly useful after add and
+           newline=False use, to finish a given line.
+        """
+        self._page = self._page + str
+
+    def opn(self, line, newline=True):
+        """Used to open an html tag, adding a line containing the tag, and
+           indenting the following text so it reflect the opened tag in the
+           indentation.
+        """
+        self.add(line, newline)
+        self.indent()
+
+    def cls(self, line, newline=True):
+        """Used to close a html tag, deindenting the text, then adding a line
+           normally containing the closing tag.
+        """
+        self.deindent()
+        self.add(line, newline)
+
+    def get(self):
+        """Return the current page text"""
+        self.cls("</body>")
+        self.cls("</html>")
+        return self._page
+
+    def __init__(self, lang="en"):
+        """ Prepare header for the page """
+        self._lang = lang
+        self.add("<!DOCTYPE %s>" % (self._doctype))
+        self.opn('<html lang="%s">' % (self._lang))
+        self.opn("<head>")
+        self.add('')
+        self.add("<title>%s</title>" % (self._title))
+        self.add('')
+        self.add('<meta http-equiv="Content-Type" content="%s; charset=%s"/>' %
+                 (self._content, self._charset))
+
+        # adding keywords
+        self.add('<meta name="keywords" content="', newline=False)
+        start = True
+        for k in self._keywords:
+            if start == False:
+                self.app(', ') 
+            else:
+                start = False
+            self.app('%s' % (k))
+        self.app('" />\n')
+
+        # add description
+        self.add('<meta name="description" content="%s" />' % (self._description[self._lang]))
+
+        # stylesheet
+        self.add('<link rel="stylesheet" type="text/css" href="%s" />' % (self._css))
+
+        # additionnal headers
+        for h in self._additionnal_headers:
+            self.add("%s" % (h))
+        
+        self.add('')
+        self.cls("</head>")
+        self.add('')
+        self.opn("<body>")
--- README.md
+++ README.md
@@ -2,3 +2,94 @@ Page
 ====
 
 Page module helper to generate webpages for cherrypy and other frameworks.
+
+Introduction
+------------
+
+The idea behind this module to help the creation of page for frameworks like
+cherrypy and alike. To sum up the usage, just create a page inheriting from
+this, fill default values for what you want to change, instantiate it, add
+content (body of webpage), and .get() the page to pass it to your framework.
+
+Usage
+-----
+
+The simplest way to start is to create a class that will inherit from Page
+class, and set the various fields you want to customize.
+
+```python
+from Page import *
+
+class MyPage(Page):
+    _keywords = [ "mypage", "demo", "raccoons" ]
+    _description = {
+            'en': 'this is the english description of my webpage',
+            'fr': 'voici la description en francais de ma page web'
+    }
+    
+# instantiate a page telling the language will ben english, which is the default
+# this will set doctype, open <html> tag, create <head> and its content
+# close </head>, open <body> 
+p = MyPage(lang='fr') # page in french
+p = MyPage(lang='en') # page in english
+p = MyPage(indent=3)  # used default lang (en), and set indent to 3 spaces, instead of 4
+
+# assume we fill content of body here, we then get the page to pass
+# it to the framework, for demo print it here.
+print(p.get())
+```
+
+This will result in this:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <title>title</title>
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta name="keywords" content="" />
+        <meta name="description" content="No description" />
+        <link rel="stylesheet" type="text/css" href="/index.css" />
+        
+    </head>
+    
+    <body>
+    </body>
+</html>
+```
+
+Provided
+--------
+
+Here are the values you can set and their default values:
+
+- _doctype = "html"  -- Define the !<DOCTYPE %s> field
+- _lang = "en"  -- language of the page, for <html lang=%s> field
+- _title = "title"  -- <title>
+- _content = "text/html"  -- content type of the page
+- _charset = "utf-8"  -- charset used for the webpage
+- _keywords = []  -- list of keywords
+- _description = { 'en': "No description" }  -- dict of page description, the one matching the lang
+  set in when instantiating your class.
+- _css = "/index.css"  -- link to the css, only one supported for now
+- _additionnal_headers = []  -- list of full custom lines to be added in the <head>
+
+And here are the methods that can be used on your class to fill the webpage:
+
+
+- indent(): Indent text that will be inserted with the insertion methods and the amount of spaces
+            defined by the instance.
+- deindent(): Remove a level of indent to insertion methods
+- add(line, newline=True): Add an indented line with current indentation
+    - line:    The line to be added
+    - newline: Defines if newline is to be inserted, default to true
+- app(str): Append a string to the current line, mostly useful after using the add plus
+            newline=False to finish a started line
+- opn(line, newline=True): Used to open an html tag, adding a line containing the tag, and indenting
+                           the following text so it reflect the opened tag in the indentation.
+- cls(line, newline=True): Used to close a html tag, deindenting the text, then adding a line
+                           normally containing the closing tag.
+- get(): Close </body> and </html>, and returns the page as it is
+
